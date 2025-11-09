edd35255d447d5e5d5ee466a37d6face877e6de1
pumpkynn <19198402685@163.com>
2025-11-09 13:43:40 +0800

    test: 验证修复

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      prettier: require('eslint-plugin-prettier'),
+      import: require('eslint-plugin-import'),
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'import/order': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+    },
+  },
+];
